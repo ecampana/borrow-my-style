5f20cadf99aee2490627881587d2c00349150903
Emmanuel Contreras-Campana <emmanuelc82@gmail.com>
2018-02-13 21:21:32 -0500

    Finalizing data-cleaning-pipeline.ipynb

---
+++ dskit/__init__.py
@@ -0,0 +1,5 @@
+# Import dskit library
+from dskit.utilities import (plot_overfitting, plot_learning_curve,
+                       grid_search, summary_report)
+
+from dskit.brands import brand_names
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from codecs import open
+from os import path
+from distutils.core import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='dskit', # name pip uses to uninstall, update, etc.. must be the same as package name
+      version='1.0',
+      description='Data Science Toolkit',
+      long_description=long_description,
+      author='Emmanuel Contreras-Campana',
+      author_email='emmanuelc82@gmail.com',
+      url='https://github.com/ecampana/borrow-my-style',
+      packages=['dskit'] # directories in borrow-my-style to install
+)
